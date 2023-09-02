c8c78453a0ec57c6ed4cc0321ab781ef07bcc062
Nnachi Kingsley <chimakingsley216@gmail.com>
2023-09-02 15:35:58 +0100

    successfull enable user to create prompt

--- components/Form.jsx
+++ components/Form.jsx
@@ -7,7 +7,7 @@ const Form = ({type, post, setPost, submitting, handleSubmit}) => {
             <span className="blue_gradient">{type} Post</span>
         </h1>
         <p className="desc text-left max-w-md">
-            Discover {type} and share amazing prompts with the world, and let your imagination run wild with any AI-platform.
+            Discover, {type} and share amazing prompts with the world, and let your imagination run wild with any AI-platform.
         </p>
 
         <form
@@ -21,11 +21,38 @@ const Form = ({type, post, setPost, submitting, handleSubmit}) => {
                 <textarea
                     value={post.prompt}
                     onChange={(e) => setPost({ ...post, prompt: e.target.value })}
-                    placeholder="Your Prompt goes here"
+                    placeholder="Your Prompt goes here..."
                     required 
                     className="form_textarea"
                     />
             </label>
+            <label>
+                <span className="font-satoshi font-semibold text-base text-gray-700">
+                    Tag {' '}
+                    <span className="font-normal">(#livestyle, #webdevelopment, #cryptocurrency)</span>
+                </span>
+                <input
+                    value={post.tag}
+                    onChange={(e) => setPost({ ...post, tag: e.target.value })}
+                    placeholder="#tag"
+                    required 
+                    className="form_input "
+                    />
+            </label>
+
+            <div className="flex-end mx-3 mb-5 gap-5">
+                <Link href='/' className="text-grey-500 text-sm">
+                    Cancel
+                </Link>
+
+                <button
+                type="submit"
+                disabled={submitting}
+                className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+                >
+                    {submitting ? `${type}...` : type}
+                </button>
+            </div>
         </form>
     </section>
   )
